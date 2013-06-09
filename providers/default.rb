3a1314a663c7ec320233f06925af0fa742d4c1bb
Kra Larivain <olarivain@gmail.com>
2013-06-09 16:04:38 -0700

    - fixed multi repo support

--- providers/default.rb
+++ providers/default.rb
@@ -38,20 +38,21 @@ def download_artifact (new_resource)
 
 		# resolve download url and fetch the content
 		artifact_url = resolve_version_url(new_resource, repository)
-		download = remote_file file_name do
-			source artifact_url
-			mode 0644
-		end
-
-		# run the download and notifiy if updated
 		begin 
+			download = remote_file file_name do
+				source artifact_url
+				mode 0644
+			end
+
+			# run the download and notifiy if updated
 			download.run_action(:create)
-		rescue e
-			Chef::Log.info("Couldn't fetch #{new_resource.group_id}:#{new_resource.artifact_id}:#{new_resource.version} from #{repository}: #{e}")
+		rescue Exception => e
+			Chef::Log.info("Couldn't fetch #{new_resource.group_id}:#{new_resource.artifact_id}:#{new_resource.version} from #{repository}.")
+			next
 		end
 		new_resource.updated_by_last_action(true) unless !download.updated_by_last_action?
 		Chef::Log.info("Fetched #{new_resource.group_id}:#{new_resource.artifact_id}:#{new_resource.version} from #{repository}.")
-		sucess = true
+		success = true
 		break
 	}
 	raise "Couldn't fetch #{new_resource.group_id}:#{new_resource.artifact_id}:#{new_resource.version} from any repository." unless success
